caf9b28033be55a7929deb1f251c059448e7e348
rfairclough <rfairclough@ign.com>
2012-08-07 01:36:26 +0400

    Live now updates

--- schedule/js/schedule.js
+++ schedule/js/schedule.js
@@ -93,11 +93,6 @@
 			    				date = moment(add_day).local().format('MMMM Do');
 			    				$('#day-7').html(day);
 			    				$('#date-7').html(date);
-			    				// subtitle_1 = match_Obj.subtitle_1;
-			    				// if (subtitle_1 == null || subtitle_1 == 'NA') subtitle_1 = '&nbsp';
-			    				// subtitle_2 = match_Obj.subtitle_2;
-			    				// if (subtitle_2 == null || subtitle_2 == 'NA') subtitle_2 = '&nbsp';
-
 			    			};
 
 			    			function getSubtitles(match_Obj) {
@@ -129,7 +124,9 @@
 			    				
 				    				if(gameTitle == 'league-of-legends') {
 				    					(function() {
-					    					if(gameEnd > now && gameStart < today) {
+				    						if(now > gameStart && now < gameEnd) {
+				    							$("<div class='listing live-now'><p><span>" + gameStart.local().format('hA') + '</span> - ' + title + "</p><p class='sub1'>" + subtitle_1 + "</p><p class='sub2'>" + subtitle_2 + "</p><p class='tags'><span class='live'><a href='http://ign.com/ipl/tv' class='now'>LIVE NOW</a></span><span class=" + tag + '>' + tag + '</span></p></div>').appendTo('#lol-today');
+					    					} else if(gameEnd > now && gameStart < today) {
 												$("<div class='listing'><p><span>" + gameStart.local().format('hA') + '</span> - ' + title + "</p><p class='sub1'>" + subtitle_1 + "</p><p class='sub2'>" + subtitle_2 + "</p><p class='tags'><span class=" + tag + '>' + tag + '</span></p></div>').appendTo('#lol-today');
 											} else if(gameStart > today && gameStart < tomorrow){
 												$("<div class='listing'><p><span>" + gameStart.local().format('hA') + '</span> - ' + title + "</p><p class='sub1'>" + subtitle_1 + "</p><p class='sub2'>" + subtitle_2 + "</p><p class='tags'><span class=" + tag + '>' + tag + '</span></p></div>').appendTo('#lol-tomorrow');
@@ -147,41 +144,38 @@
 										})();
 									} else {
 										(function() {
-					    					if(gameEnd > now && gameStart < today) {
-												$("<div class='listing'><p><span>" + gameStart.local().format('hA') + '</span> - ' + title + "</p><p class='sub'>" + subtitle_1 + '</p><p><span class=' + tag + '>' + tag + '</span></p></div>').appendTo('#sc-today');
+											if(now > gameStart && now < gameEnd) {
+				    							$("<div class='listing live-now'><p><span>" + gameStart.local().format('hA') + '</span> - ' + title + "</p><p class='sub1'>" + subtitle_1 + "</p><p class='sub2'>" + subtitle_2 + "</p><p class='tags'><span class='live'><a href='http://ign.com/ipl/tv' class='now'>LIVE NOW</a></span><span class=" + tag + '>' + tag + '</span></p></div>').appendTo('#sc-today');
+					    					} else if(gameEnd > now && gameStart < today) {
+												$("<div class='listing'><p><span>" + gameStart.local().format('hA') + '</span> - ' + title + "</p><p class='sub'>" + subtitle_1 + "</p><p class='sub2'>" + subtitle_2 + "</p><p class='tags'><span class=" + tag + '>' + tag + '</span></p></div>').appendTo('#sc-today');
 											} else if(gameStart > today && gameStart < tomorrow){
-												$("<div class='listing'><p><span>" + gameStart.local().format('hA') + '</span> - ' + title + "</p><p class='sub'>" + subtitle_1 + '</p><p><span class=' + tag + '>' + tag + '</span></p></div>').appendTo('#sc-tomorrow');
+												$("<div class='listing'><p><span>" + gameStart.local().format('hA') + '</span> - ' + title + "</p><p class='sub'>" + subtitle_1 + "</p><p class='sub2'>" + subtitle_2 + "</p><p class='tags'><span class=" + tag + '>' + tag + '</span></p></div>').appendTo('#sc-tomorrow');
 											} else if(gameStart > tomorrow && gameStart < day3){
-												$("<div class='listing'><p><span>" + gameStart.local().format('hA') + '</span> - ' + title + "</p><p class='sub'>" + subtitle_1 + '</p><p><span class=' + tag + '>' + tag + '</span></p></div>').appendTo('#sc-3');
+												$("<div class='listing'><p><span>" + gameStart.local().format('hA') + '</span> - ' + title + "</p><p class='sub'>" + subtitle_1 + "</p><p class='sub2'>" + subtitle_2 + "</p><p class='tags'><span class=" + tag + '>' + tag + '</span></p></div>').appendTo('#sc-3');
 											} else if(gameStart > day3 && gameStart < day4){
-												$("<div class='listing'><p><span>" + gameStart.local().format('hA') + '</span> - ' + title + "</p><p class='sub'>" + subtitle_1 + '</p><p><span class=' + tag + '>' + tag + '</span></p></div>').appendTo('#sc-4');
+												$("<div class='listing'><p><span>" + gameStart.local().format('hA') + '</span> - ' + title + "</p><p class='sub'>" + subtitle_1 + "</p><p class='sub2'>" + subtitle_2 + "</p><p class='tags'><span class=" + tag + '>' + tag + '</span></p></div>').appendTo('#sc-4');
 											} else if(gameStart > day4 && gameStart < day5){
-												$("<div class='listing'><p><span>" + gameStart.local().format('hA') + '</span> - ' + title + "</p><p class='sub'>" + subtitle_1 + '</p><p><span class=' + tag + '>' + tag + '</span></p></div>').appendTo('#sc-5');
+												$("<div class='listing'><p><span>" + gameStart.local().format('hA') + '</span> - ' + title + "</p><p class='sub'>" + subtitle_1 + "</p><p class='sub2'>" + subtitle_2 + "</p><p class='tags'><span class=" + tag + '>' + tag + '</span></p></div>').appendTo('#sc-5');
 											} else if(gameStart > day5 && gameStart < day6){
-												$("<div class='listing'><p><span>" + gameStart.local().format('hA') + '</span> - ' + title + "</p><p class='sub'>" + subtitle_1 + '</p><p><span class=' + tag + '>' + tag + '</span></p></div>').appendTo('#sc-6');
+												$("<div class='listing'><p><span>" + gameStart.local().format('hA') + '</span> - ' + title + "</p><p class='sub'>" + subtitle_1 + "</p><p class='sub2'>" + subtitle_2 + "</p><p class='tags'><span class=" + tag + '>' + tag + '</span></p></div>').appendTo('#sc-6');
 											} else if(gameStart > day6 && gameStart < day7){
-												$("<div class='listing'><p><span>" + gameStart.local().format('hA') + '</span> - ' + title + "</p><p class='sub'>" + subtitle_1 + '</p><p><span class=' + tag + '>' + tag + '</span></p></div>').appendTo('#sc-7');
+												$("<div class='listing'><p><span>" + gameStart.local().format('hA') + '</span> - ' + title + "</p><p class='sub'>" + subtitle_1 + "</p><p class='sub2'>" + subtitle_2 + "</p><p class='tags'><span class=" + tag + '>' + tag + '</span></p></div>').appendTo('#sc-7');
 											};
 										})();
 									};
 
-									// if (title == 'IPL Face Off: San Francisco Showdown') {
-									// 	console.log(title);
-									// 	$("<span class='live'><a href='http://www.ign.com/ipl/all/face-off-san-francisco-showdown' class='now'>FACE OFF</a></span>").prependTo('.tags');
+									// if(now > gameStart && now < gameEnd) {
+									// 	$("<span class='live'><a href='http://ign.com/ipl/tv' class='now'>LIVE NOW</a></span>").prependTo('.tags');
+									// 	$(".listing").click(function() {
+								 //     		window.location=$(this).find("a").attr("href"); 
+								 //     		return false;
+									// 	});
+									// 	$('.listing').on('mouseenter', function() {
+									// 		$(this).addClass('hover').on('mouseleave', function() {
+									// 			$(this).removeClass('hover');
+									// 		});
+									// 	});
 									// };
-
-									if(now > gameStart && now < gameEnd) {
-										$("<span class='live'><a href='http://ign.com/ipl/tv' class='now'>LIVE NOW</a></span>").prependTo('.tags');
-										$(".listing").click(function() {
-								     		window.location=$(this).find("a").attr("href"); 
-								     		return false;
-										});
-										$('.listing').on('mouseenter', function() {
-											$(this).addClass('hover').on('mouseleave', function() {
-												$(this).removeClass('hover');
-											});
-										});
-									};
 								};
 							};
 		    			};
