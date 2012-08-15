1a2b33b8e9c3c209d4be5dfc8b5daf4acd2139dd
Paul Grock <paulgrock@gmail.com>
2012-08-15 10:32:16 -0700

    refactored scoreboard js and css

--- scores/js/scores.js
+++ scores/js/scores.js
@@ -1,133 +1,149 @@
 (function($) {
 	var basepath = "";
 	if (typeof local === "undefined") {
-		basepath = "http://esports.ign.com/addons/ipl-widgets/scores/";
+		basepath = "http://test.ign.com/addons/ipl-widgets/scores/";
 	}
 	var ipl = {
 		init: function(config) {
 			this.loadStyleSheet();
 			this.url = 'http://esports.ign.com/scores.json';
-			this.template = config.template;
-			this.container = config.container;
+			this.container = $('#scores');
 			this.fetch();
 			this.buttons();
 		},
+		scoresTmpl: function(){
+			var html = "";
+			html += "<div class='controls'><div class='left-control'><img src='http://esports.ign.com/addons/ipl-widgets/scores/images/left.png' class='left-button' /></div><div class='container'><div class='box-scores'>";
+			for (var i = this.matchUps.length - 1; i >= 0; i--) {
+				console.log();
+				html += "<div class='match'>";
+				html += "<div class='date-container'><div class='date'>" + this.matchUps[i].date + "</div></div>";
+				html += "<div class='title'>" + this.matchUps[i].title + "</div>";
+				html += "<div class='team-top'><div class='team-name " + this.matchUps[i].team1Class + "'>" + this.matchUps[i].username1 +"</div><div class='score " + this.matchUps[i].team1Class + "'>" + this.matchUps[i].points1 + "</div></div>";
+				html += "<div class='team-bottom'><div class='team-name " + this.matchUps[i].team2Class + "'>" + this.matchUps[i].username2 +"</div><div class='score " + this.matchUps[i].team2Class + "'>" + this.matchUps[i].points2 + "</div></div>";
+				if (this.matchUps[i].url !== "#") {
+					html += "<div class='status-container'><a href='" + this.matchUps[i].url + "' class='status-link " + this.matchUps[i].status + "'>" + this.matchUps[i].status + "</a></div>";
+				} else {
+					html += "<div class='status-container'><p class='status " + this.matchUps[i].status + "'>" + this.matchUps[i].status + "</p></div>";
+				}
 
-		attachTemplate: function() {
-			var template = Handlebars.compile(this.template);
-			this.container.append(template(this.matchUps));
+				html += "</div>";
+			}
+			html += "</div></div><div class='right-control'><img src='http://esports.ign.com/addons/ipl-widgets/scores/images/right.png' class='right-button' /></div></div>";
+			return html;
 		},
-
-		fetch: function() {
+		attachTemplate: function(){
+			this.container.append(this.scoresTmpl());
+		},
+		getTitle: function(title) {
+			if(title === "") {
+				return "&nbsp;";
+			} else {
+				return title;
+			}
+		},
+		getStatus: function(status, url){
+			switch (status)
+			{
+			case 'finished':
+				if(url === null) {
+					return "COMING SOON";
+				} else {
+					return "WATCH VOD";
+				}
+					break;
+			case 'underway':
+				return "LIVE NOW";
+			case 'ready':
+				return "COMING SOON";
+			default:
+				return "FINISHED";
+			}
+		},
+		getURLs: function(url, status, franchiseSlug){
+			if (status == 'underway') {
+				return 'http://ign.com/ipl/' + franchiseSlug;
+			} else if (status == 'finished') {
+				if(url === null) {
+					return '#';
+				} else {
+					return 'http://ign.com' + url;
+				}
+			} else {
+				return '#';
+			}
+		},
+		fetch: function(url, status, slug) {
 			var self = this;
 			$.ajax({
-			    type: 'GET',
-			    url: this.url,
-			    dataType: "jsonp",
-			    cache: true,
+					type: 'GET',
+					url: this.url,
+					dataType: "jsonp",
+					cache: true,
 				jsonpCallback: "getCachedScores",
 
-			    success: function(data) {
-			        self.matchUps = [];
-			        $.map(data, function(data, date) {
-				        $.map(data, function(match) {
-				        	var game = {
-				        		date: moment(date, "YYYY-MM-DD").format("MMM D, YYYY"),
-				        		title: (function (title) {
-				        			if(title == "") {
-				        				return "&nbsp;";
-					        		} else {
-					        			return title;
-					        		}
-					        	})(match.match_score.title),
-				        		status: (function(status, url) {
-				        			switch (status)
-									{
-									case 'finished':
-										if(url == null) {
-									  	return "COMING SOON";
-									  } else {
-									  	return "WATCH VOD";
-									  };
-									  	break;
-									case 'underway':
-										return "LIVE NOW";
-										break;
-									case 'ready':
-										return "COMING SOON";
-										break;
-									default:
-									  	return "FINISHED";
-									}
-				        		})(match.match_score.match.status, match.match_score.match.url),
-				        		url: (function(url, status, slug) {
-				        			if (status == 'underway') {
-				        				return 'http://ign.com/ipl/' + slug;
-									} else if (status == 'finished') {
-										if(url == null) {
-											return '#';
-										} else {
-											return 'http://ign.com' + url;
-										};
-									} else {
-										return '#';
+					success: function(data) {
+							self.matchUps = [];
+							$.map(data, function(data, date) {
+								$.map(data, function(match) {
+									var game = {
+										date: moment(date, "YYYY-MM-DD").format("MMM D, YYYY"),
+										title: self.getTitle(match.match_score.title),
+										status: self.getStatus(match.match_score.match.status, match.match_score.match.url),
+										url: self.getURLs(match.match_score.match.url, match.match_score.match.status, match.match_score.match.show.franchise.slug)
 									};
-				        		})(match.match_score.match.url, match.match_score.match.status, match.match_score.match.show.franchise.slug)
-				        	};
-				        	var i = 1;
-				        	var team1Score,
-				        		team2Score,
-				        		team1Class,
-				        		team2Class;
-				        	$.map(match.match_score.card, function(teamScore) {
-				        		if (i == 1) team1Score = teamScore.points;
-				        		if (i == 2) team2Score = teamScore.points;
-				        		i++;
-				        	});
-				        	if (team1Score > team2Score) {
-				        		team1Class = 'winner';
-				        		team2Class = 'loser';
-				        	} else if (team1Score < team2Score) {
-				        		team2Class = 'winner';
-				        		team1Class = 'loser';
-				        	} else {
-				        		team1Class = 'draw';
-				        		team2Class = 'draw';
-				        	}
-				        	i = 1;	
-				        	$.map(match.match_score.card, function(teamScore) {
-				        		game['username' + i] = teamScore.username;
-				        		game['points' + i] = teamScore.points;
-				        		if (i == 1) game.team1Class = team1Class;
-				        		if (i == 2) game.team2Class = team2Class;
-				        		i++;
-				        	});
+									var i = 1;
+									var team1Score,
+										team2Score,
+										team1Class,
+										team2Class;
+									$.map(match.match_score.card, function(teamScore) {
+										if (i == 1) team1Score = teamScore.points;
+										if (i == 2) team2Score = teamScore.points;
+										i++;
+									});
+									if (team1Score > team2Score) {
+										team1Class = 'winner';
+										team2Class = 'loser';
+									} else if (team1Score < team2Score) {
+										team2Class = 'winner';
+										team1Class = 'loser';
+									} else {
+										team1Class = 'draw';
+										team2Class = 'draw';
+									}
+									i = 1;
+									$.map(match.match_score.card, function(teamScore) {
+										game['username' + i] = teamScore.username;
+										game['points' + i] = teamScore.points;
+										if (i === 1) game.team1Class = team1Class;
+										if (i === 2) game.team2Class = team2Class;
+										i++;
+									});
 
-				        	self.matchUps.push(game);
-				        });
+									self.matchUps.push(game);
+								});
 					});
 					self.attachTemplate();
-			    },
-			    error: function(jqXHR, textStatus, errorThrown) {
-			        data = {
-			            error: true,
-			            jqXHR: jqXHR,
-			            textStatus: textStatus,
-			            errorThrown: errorThrown
-			        };
-			        callback.call(this, data)
-			    }
+					},
+					error: function(jqXHR, textStatus, errorThrown) {
+							data = {
+									error: true,
+									jqXHR: jqXHR,
+									textStatus: textStatus,
+									errorThrown: errorThrown
+							};
+							callback.call(this, data);
+					}
 			});
 		},
 
 		loadStyleSheet: function() {
-		   var head = document.getElementsByTagName( 'head' )[0], // reference to document.head for appending/ removing link nodes
-		       link = document.createElement( 'link' );           // create the link node
-
-		   link.setAttribute( 'href', basepath + 'css/style.css' );
-
-		   link.setAttribute( 'rel', 'stylesheet' );
-		   head.appendChild(link);  // insert the link node into the DOM and start loading the style sheet
+			var head = document.getElementsByTagName( 'head' )[0],
+					link = document.createElement( 'link' );
+			link.setAttribute( 'href', basepath + 'css/style.css' );
+			link.setAttribute( 'rel', 'stylesheet' );
+			head.appendChild(link);
 		},
 
 		buttons: function() {
@@ -143,62 +159,35 @@
 			}).on('mouseleave', '.right-button', function() {
 				$(this).animate({'margin-left': '-=5'}, 300);
 			});
-					 
+
 			var i = 1;
 
 			$('#scores').on('click', '.right-button', function() {
-				if(i == 5) {
+				if(i === 5) {
 					$('.box-scores').css('margin-left', '-760');
 				} else {
 					$('.box-scores').animate({'margin-left': '-=152'}, 500);
 					i++;
-				};
+				}
 			});
 
 			$('#scores').on('click', '.left-button', function() {
-				if(i == 1) {
+				if(i === 1) {
 					$('.box-scores').css('margin-left', '0');
 				} else {
 					$('.box-scores').animate({'margin-left': '+=152'}, 500);
 					i--;
-				};
+				}
 			});
 
 			// Make entire div clickable
-			$(".status-container").click(function(){
-	     		window.location=$(this).find("a").attr("href"); 
-	     		return false;
+			$(".status-container").click(function(evt){
+					evt.preventDefault();
+					window.location = $(this).find("a").attr("href");
 			});
 
 		}
-
-		// loadBox: function() {
-		//    var box = document.getElementByID( 'box-scores' )[0], // reference to document.head for appending/ removing link nodes
-		//        script = document.createElement( 'script' );           // create the link node
-		//    script.setAttribute( 'src', 'box.js' );
-		//    script.setAttribute( 'id', 'ipl-box-scores-template');
-		//    script.setAttribute( 'type', 'ipl/template');
-		//    box.appendChild(script);  // insert the link node into the DOM and start loading the style sheet
-		// },
-
-		// loadScores: function() {
-		// 	var script = document.createElement('script');
-		// 	script.type = 'text/javascript';
-		// 	script.id = 'ipl-box-scores-template'
-		// 	script.src = 'box.js';
-
-		// 	$(".box-scores").append();
-		// }
-
-		// appendHTML: function() {
-		// 	alert(document.body.innerHTML);
-		// }
 		
 	};
-
-	ipl.init({
-		template: $('#' + scoresObj.id).html(),
-		container: $('#scores'),
-	});
-
+	ipl.init();
 })(jQuery);
